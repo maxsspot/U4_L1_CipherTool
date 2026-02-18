22bccc4319b3e2344f437e5a7d9a9d66b3abfbe0
Max <maxsnyder12345@gmail.com>
2026-02-18 14:53:52 -0500

    day #2

--- index.js
+++ index.js
@@ -1,19 +1,98 @@
 let options=document.getElementsByClassName("option");
-let selected=document.getElementById("selected");
-let textbox=document.getElementById("text");
-let actionButton=document.getElementById("fixText");
+let actionButton=document.getElementById("fixText")
+let mode="encrypt"
+let open=false
+let learnControls=document.getElementsByClassName("learnControls");
 
-let mode="encrypt";
+options[0].addEventListener('click',encryptMode)
+options[1].addEventListener('click',decryptMode)
+actionButton.addEventListener('click',fixText)
+learnControls[0].addEventListener('click',openCloseLearn)
+learnControls[1].addEventListener('click',openCloseLearn)
 
-options[0].onclick=function() {
+// Changes mode to encrypt
+function encryptMode() {
     options[0].style.fontWeight="bold";
     options[1].style.fontWeight="normal";
-    textbox.placeholder="Type your regular text here..."
+    text.placeholder="Type your regular text here..."
     actionButton.textContent="Encrypt Text"
+    mode="encrypt"
 }
-options[1].onclick=function() {
+
+// Changes mode to decrypt
+function decryptMode() {
     options[0].style.fontWeight="normal";
     options[1].style.fontWeight="bold";
-    textbox.placeholder="Type your encrypted text here...";
+    text.placeholder="Type your encrypted text here...";
     actionButton.textContent="Decrypt Text"
+    mode="decrypt"
+}
+
+// Determines weather to open or close learn area
+function openCloseLearn() {
+    let learnArea=document.getElementById("explanation");
+    if (!open) {
+        openLearn(learnArea)
+    } else {
+        closeLearn(learnArea)
+    }
+}
+
+// Opens learn area
+function openLearn(learnArea){
+    learnArea.style.opacity="1";
+    learnArea.style.pointerEvents="all";
+    open=true;
+}
+
+// Closes learn area
+function closeLearn(learnArea) {
+    learnArea.style.opacity="0";
+    learnArea.style.pointerEvents="none";
+    open=false;
+}
+
+// Decides waht to do with the text
+function fixText() {
+    const alphabet=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+    let text=document.getElementById("text");
+    let original=text.value
+
+    if(mode=="encrypt"){
+        encrypt(original, alphabet)
+    } else{
+        decrypt(original, alphabet)
+    }
+}
+
+// Encrypts the text
+function encrypt(original,alphabet){
+    let encrypted=""
+    for (i in original) {
+        if(alphabet.includes(original[i])) {
+            if((alphabet.indexOf(original[i])+1) < alphabet.length) {
+                encrypted=encrypted+alphabet[(alphabet.indexOf(original[i]))+1]
+            }else{
+                encrypted=encrypted+alphabet[1]
+            }
+        }else{
+            encrypted=encrypted+original[i]
+        }
+    }
+    text.value=""
+    text.value+=encrypted
+}
+
+// Decrypts the text
+function decrypt(original,alphabet){
+    let decrypted=""
+    for (i in original) {
+        if(alphabet.includes(original[i])) {
+            decrypted=decrypted+alphabet[(alphabet.indexOf(original[i]))-1]
+        }else{
+            decrypted=decrypted+original[i]
+        }
+    }
+    text.value=""
+    text.value+=decrypted
 }
